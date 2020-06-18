2e4f27bcdd8d8cbd3a78c391584fd1605a9e82ed
jangboo <janghyuk125@hotmail.com>
2020-06-18 12:08:13 -0400

    Edited portfile and cmakelist

--- ports/lzma/portfile.cmake
+++ ports/lzma/portfile.cmake
@@ -1,17 +1,20 @@
 
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO JangBoo/lzma
-    REF master
-    SHA512 f8ce33d366d03df9c78512bd523791ec809bea4c36bc4771b3ee631b2b920ce1ceac4149465cb27d8929307552614f860c65b61026b09fbd4ee88a9367463703
-    HEAD_REF master
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://www.7-zip.org/a/lzma1900.7z"
+    FILENAME "lzma-19.00.zip"
+    SHA512 60532483249d86960d5c481dc88ebeafabb7e435afb6188bc26f38177b8534ef3da83317d6920d99103d4f10a8dcdb624489ea5ddad8e0c8732bab1bb4ebd7eb
 )
-
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    NO_REMOVE_ONE_LEVEL
+    PATCHES ${PATCHES}
+)
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA 
-
 )
 
 vcpkg_install_cmake()
@@ -19,7 +22,7 @@ vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-SET(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
-SET(VCPKG_POLICY_DLLS_WITHOUT_EXPORTS enabled)
+set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
+set(VCPKG_POLICY_DLLS_WITHOUT_EXPORTS enabled)
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib" "${CURRENT_PACKAGES_DIR}/lib")
 configure_file(${CMAKE_CURRENT_LIST_DIR}/LICENSE ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
